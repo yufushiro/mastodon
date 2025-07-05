997a408c213613a15aced0d3ece118af19c0e577
yufushiro <62991447+yufushiro@users.noreply.github.com>
2025-07-06 03:05:13 +0900

    [yufushiro] legacy quote でも常に有効な引用として表示する

--- app/serializers/rest/base_quote_serializer.rb
+++ app/serializers/rest/base_quote_serializer.rb
@@ -4,6 +4,7 @@ class REST::BaseQuoteSerializer < ActiveModel::Serializer
   attributes :state
 
   def state
+    return 'legacy' if !object.accepted? && object.legacy?
     return object.state unless object.accepted?
 
     # Extra states when a status is unavailable
@@ -13,7 +14,7 @@ class REST::BaseQuoteSerializer < ActiveModel::Serializer
   end
 
   def quoted_status
-    object.quoted_status if (object.accepted? || instance_options[:source_requested]) && object.quoted_status.present? && !object.quoted_status&.reblog? && status_filter.filter_state_for_quote != 'unauthorized'
+    object.quoted_status if (object.accepted? || object.legacy? || instance_options[:source_requested]) && object.quoted_status.present? && !object.quoted_status&.reblog? && status_filter.filter_state_for_quote != 'unauthorized'
   end
 
   private
